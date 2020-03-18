9d549e8a1f93d00cbf083f5117acc2f11445aee3
Sivantha Paranavithana <madusham3@gmail.com>
2020-03-18 14:54:29 +0530

    Add lock

--- test/test_10_multiprocessing_sound.py
+++ test/test_10_multiprocessing_sound.py
@@ -2,7 +2,7 @@ from multiprocessing import Process
 import time
 import pyaudio
 import audioop
-from multiprocessing import Pool
+from multiprocessing import Pool, Lock
 
 # time delay between each process ~ 0.0003s
 
@@ -15,33 +15,45 @@ dev_2 = 3
 dev_3 = 4
 audio = pyaudio.PyAudio()
 
-def listen(dev):
-    stream = audio.open(format = sampling_format, rate = sampling_rate, channels = sampling_channels, \
-                        input_device_index = dev, input = True, \
-                        frames_per_buffer = chunk)
+
+def listen(dev, lock):
+    stream = audio.open(format=sampling_format, rate=sampling_rate, channels=sampling_channels,
+                        input_device_index=dev, input=True,
+                        frames_per_buffer=chunk)
     print("recording")
     if dev == 2:
         while True:
             time.sleep(0.5)
-            data = stream.read(chunk, exception_on_overflow = False)
+            data = stream.read(chunk, exception_on_overflow=False)
             rms = audioop.rms(data, 2)
-            print("\n" + str(dev-1) + " - " + str(rms))
+            lock.acquire()
+            print("\n" + str(dev - 1) + " - " + str(rms))
+            lock.release()
     elif dev == 3:
         while True:
             time.sleep(0.5)
-            data = stream.read(chunk, exception_on_overflow = False)
+            data = stream.read(chunk, exception_on_overflow=False)
             rms = audioop.rms(data, 2)
-            print(str(dev-1) + " - " + str(rms))
+            lock.acquire()
+            print(str(dev - 1) + " - " + str(rms))
+            lock.release()
     else:
         while True:
             time.sleep(0.5)
-            data = stream.read(chunk, exception_on_overflow = False)
+            data = stream.read(chunk, exception_on_overflow=False)
             rms = audioop.rms(data, 2)
-            print(str(dev-1) + " - " + str(rms))
+            lock.acquire()
+            print(str(dev - 1) + " - " + str(rms))
+            lock.release()
 
 def run_process(process):
     process.start()
-    
+
+def init(l):
+    global lock
+    lock = l
+
 if __name__ == "__main__":
-    pool = Pool(processes = 3)
+    l = Lock()
+    pool = Pool(processes=3, initializer=init, initargs=(l,))
     pool.map(listen, [dev_1, dev_2, dev_3])
