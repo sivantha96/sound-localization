dbf6123afb0e05a8c405573972365bbf67b112d6
sivantha96 <madusham3@gmail.com>
2020-08-29 22:40:22 +0530

    edited from pi

--- model/testing_model.py
+++ model/testing_model.py
@@ -12,16 +12,27 @@ categories = pd.DataFrame(['Iris-virginica', 'Iris-setosa', 'Iris-versicolor'])
 encoder = get_encoder(categories)
 
 # open model.json
-json_file = open('model.json', 'r')
+json_file = open('/home/pi/Documents/Projects/sound-localization/model.json', 'r')
 model_json = json_file.read()
 json_file.close()
 
 # load model
 model = model_from_json(model_json)
-model.load_weights("model.h5")
+model.load_weights("/home/pi/Documents/Projects/sound-localization/model.h5")
 
 # compile the model
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+print("Model is ready")
+
+vol_A = 4.0
+vol_B = 2.2
+vol_C = 0
+
 # print the prediction
-print(predict_category(model, encoder, 5.1, 3.5, 1.4))
+while True:
+    print(vol_A, vol_B, vol_C)
+    print(predict_category(model, encoder, vol_A, vol_B, vol_C))
+    vol_A = vol_A + 0.003
+    vol_B = vol_B + 0.007
+    vol_C = vol_C + 0.01
